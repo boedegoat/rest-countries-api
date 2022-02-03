1c6583ec47e66eda65a039d2c7aeaefd1b21557c
boedegoat <bhremada.fka@gmail.com>
2022-02-03 22:07:52 +0700

    Change REST Countries API version to v3.1

--- components/Countries.tsx
+++ components/Countries.tsx
@@ -1,14 +1,19 @@
 import Link from 'next/link'
 
 export default function Countries({ countries }) {
+  console.log(countries)
+
   return (
     <div className="wrapper mx-7 my-10 space-y-10">
       {countries.map((country, id) => (
-        <Link key={id} href={`/${country.name}`}>
+        <Link key={id} href={`/${country.name.common}`}>
           <a className="block overflow-hidden rounded-md bg-light-elements shadow-md dark:bg-dark-elements">
-            <img src={country.flag} alt={`${country.name}'s flag`} />
+            <img
+              src={country.flags.svg}
+              alt={`${country.name.common}'s flag`}
+            />
             <div className="px-7 pt-5 pb-10">
-              <p className="text-lg font-extrabold">{country.name}</p>
+              <p className="text-lg font-extrabold">{country.name.common}</p>
               <div className="mt-4 space-y-1">
                 <p className="text-sm">
                   <span className="font-extrabold">Population</span>:{' '}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Form from 'components/Form'
 import Header from 'components/Header'
 import Layout from 'components/Layout'
 
+const REST_COUNTRIES_API = 'https://restcountries.com/v3.1'
+
 export default function Home({ countries }) {
   return (
     <Layout title="REST Countries">
@@ -14,7 +16,7 @@ export default function Home({ countries }) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch('https://restcountries.com/v2/all')
+  const res = await fetch(`${REST_COUNTRIES_API}/all`)
   const countries = await res.json()
   return {
     props: { countries },
