b71ae6f0f61efef77a410abff7801d735f517e54
boedegoat <bhremada.fka@gmail.com>
2022-02-06 20:00:24 +0700

    Make header h1 to be link to homepage

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import { MoonIcon } from '@heroicons/react/outline'
 import { MoonIcon as MoonIconSolid } from '@heroicons/react/solid'
 import { useTheme } from 'lib/ThemeProvider'
+import Link from 'next/link'
 
 export default function Header() {
   const { theme, setTheme } = useTheme()
@@ -9,7 +10,11 @@ export default function Header() {
   return (
     <header className="bg-light-elements py-7 shadow-md dark:bg-dark-elements">
       <div className="wrapper flex items-center justify-between">
-        <h1 className="font-extrabold">Where in the world ?</h1>
+        <Link href="/">
+          <a>
+            <h1 className="font-extrabold">Where in the world ?</h1>
+          </a>
+        </Link>
         {/* Dark Mode button */}
         <button
           className="flex items-center space-x-2"
